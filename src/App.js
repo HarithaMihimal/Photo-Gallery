9690a7c32276678b385f04931167ed5654e32e55
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2023-12-08 20:54:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Gallery from './newGallery';
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign: 'center'}}>Image Gallery</h1>
+    <h2 style={{textAlign:'center'}}>MAAL Photography PVT</h2>
+    <Gallery/>
+    </>
+  );
+}
+
+export default App;
